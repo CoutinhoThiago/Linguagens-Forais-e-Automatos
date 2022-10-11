773320e4101fcdb61e42e64f2cdd6bbb2dda6f74
coutinho_thiago <tcoutinhossilva@gmail.com>
2022-10-11 17:53:45 -0300

    aberto fechado

---
+++ Engenharia-de-software-I/PrincipioDoAbertoEFechado/src/modelo/CriadorContasDiretor.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class CriadorContasDiretor implements ICriadorContas {
+
+	@Override
+	public Empregado criar(ICandidato candidato) {
+		Empregado empregado = new Empregado();
+
+        empregado.setNome(candidato.getNome());
+        empregado.setSobrenome(candidato.getSobrenome());
+        empregado.setEmail(candidato.getNome() + candidato.getSobrenome() + "@xptodiretoria.com");
+        
+        empregado.setDiretor(true);
+        
+        return empregado;
+    }
+
+}
