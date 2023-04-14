858f2d8512d87de96d41fab95ee6618207ff8548
coutinho_thiago <55064401+CoutinhoThiago@users.noreply.github.com>
2023-04-14 19:30:58 -0300

    14-04-2023

---
+++ Qualidade-de-software/LeapYear/src/test/LeapYearTest.java
@@ -0,0 +1,17 @@
+package test;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import main.LeapYear;
+
+public class LeapYearTest {
+	@Test
+	public void testSizeStack() {
+
+	    assertEquals(3, 2);
+	}
+}
