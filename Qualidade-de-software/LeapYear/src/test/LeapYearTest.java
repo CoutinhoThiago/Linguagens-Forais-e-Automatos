ebe00d71ecbd366a044629cb5a20dfe62c9e8b1b
coutinho_thiago <55064401+CoutinhoThiago@users.noreply.github.com>
2023-04-14 19:58:09 -0300

    aula 14-04-2023

--- Qualidade-de-software/LeapYear/src/test/LeapYearTest.java
+++ Qualidade-de-software/LeapYear/src/test/LeapYearTest.java
@@ -1,17 +1,34 @@
 package test;
 
-
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import main.LeapYear;
 
 public class LeapYearTest {
+	private final LeapYear ano = new LeapYear();
+	
 	@Test
-	public void testSizeStack() {
+	public void quatro() {
+		int random = (int) Math.random();
 
-	    assertEquals(3, 2);
+		assertTrue(ano.isLeapYear(random*4));
+	}
+	@Test
+	public void errado() {
+		int random = (int) Math.random();
+
+		assertFalse(ano.isLeapYear(random*4*100));
+	}
+	
+	@Test
+	public void quatrocentos() {
+		int random = (int) Math.random();
+
+		assertTrue(ano.isLeapYear(random*400));
+
 	}
 }
